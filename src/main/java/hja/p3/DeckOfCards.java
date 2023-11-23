bf9a072c9a0a65a6e8148f841536352abff8afc8
usuario_local <usuario_local@PTO0907.fdi.ucm.es>
2023-11-23 15:37:27 +0100

    Cositas*2

--- src/main/java/hja/p3/DeckOfCards.java
+++ src/main/java/hja/p3/DeckOfCards.java
@@ -1,10 +1,25 @@
 
 package hja.p3;
 
+import java.util.ArrayList;
+
 public class DeckOfCards {
    
-    private String orderCards = "2h3h4h5h6h7h8h9hThJhQhKhAh2d3d4d5d6d7d8d9dTdJdQdKdAd2c3c4c5c6c7c8c9cTcJcQcKcAc2s3s4s5s6s7s8s9sTsJsQsKsAs";
-    public DeckOfCards(){   
+    private String orderCards;
+    private String originalCards;
+    public DeckOfCards(){
+        orderCards = "2h3h4h5h6h7h8h9hThJhQhKhAh2d3d4d5d6d7d8d9dTdJdQdKdAd2c3c4c5c6c7c8c9cTcJcQcKcAc2s3s4s5s6s7s8s9sTsJsQsKsAs";
+    }
+    
+    public DeckOfCards(ArrayList<Card> cardList){
+        orderCards = "2h3h4h5h6h7h8h9hThJhQhKhAh2d3d4d5d6d7d8d9dTdJdQdKdAd2c3c4c5c6c7c8c9cTcJcQcKcAc2s3s4s5s6s7s8s9sTsJsQsKsAs";
+        for(int i= 0; i < cardList.size(); i++){
+            orderCards.replace(cardList.get(i).toString(), "");
+        }
+        originalCards = orderCards;
+    }
+    public void reset(){
+        orderCards = originalCards;
     }
     
     public Card dealingCard(){
@@ -23,5 +38,7 @@ public class DeckOfCards {
         }
         return  new Card(randomCards.charAt(0)+"",randomCards.charAt(1)+"");             
     }
+    
+    
     
 }
--- src/main/java/hja/p3/Game.java
+++ src/main/java/hja/p3/Game.java
@@ -1,24 +1,29 @@
 package hja.p3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class Game {
     
     private final int N_PLAYERS = 6;
-    private ArrayList<Player> PlayerList;
-    private ArrayList<Double> PlayerEquityList;
-    private Board board;
+    private ArrayList<Player> PlayerList = new ArrayList<>();
+    private ArrayList<Double> PlayerEquityList = new ArrayList<>();
     private DeckOfCards deck;
-    private int N_SIMULATIONS = 200;
+    private int N_SIMULATIONS = 20000;
     
     public Game(){
         board = new Board();
         deck = new DeckOfCards();
+        System.out.println("");
         for(int i = 0; i < N_PLAYERS; i++){
             PlayerList.add(new Player("J" + i));
             PlayerList.get(i).addCard(deck.dealingCard());
-            PlayerList.get(i).addCard(deck.dealingCard());   
+            PlayerList.get(i).addCard(deck.dealingCard());  
+            System.out.println("J" + i);
+            System.out.println(PlayerList.get(i).getPlayerCards().get(0).toString() + " " +PlayerList.get(i).getPlayerCards().get(1).toString());
         }
+
     }
     
     public void drawCards(int n){
@@ -29,23 +34,43 @@ public class Game {
     
     public ArrayList<Double> getEquity(){
         ArrayList<Double> playerWins = new ArrayList<>();
-        ArrayList<Boolean> playerTie;
+        for (int i = 0; i < N_PLAYERS; i++) {
+            playerWins.add(0.0);
+        }
+        Stack<Integer> playerTie = new Stack<Integer>();
         int bestPlayer = -1;
-        double bestHand = 0;
-        int currHand = 0;
+        Hand bestHand;
+        Hand currHand;
         for(int i = 0; i < N_SIMULATIONS; i++){
-            
+            Board board = new Board();
             for(int j = 0; j < 5; j++){
                 board.addBoardCard(deck.dealingCard());
             }
-            for(int k = 0; k < N_PLAYERS; k++){
-                PlayerList.get(i).getBestHand();
-                if(currHand > bestHand){
-                    
+            playerTie.push(0);
+            bestHand = PlayerList.get(0).getBestHand();
+            for(int k = 1; k < N_PLAYERS; k++){
+                currHand = PlayerList.get(k).getBestHand();
+                if(currHand.compareTo(bestHand) == 1){
+                    bestHand = currHand;
+                    playerTie.removeAllElements();
+                    playerTie.push(k);
                 }
+                if(currHand.compareTo(bestHand) == 0){
+                    playerTie.push(k);
+                }
+            }
+            int nWins = playerTie.size();
+            int aux;
+            while(!playerTie.empty()){
+                aux = playerTie.pop();
+                playerWins.set(aux, playerWins.get(aux)+(1.0/nWins));
             }
             
         }
+        for(int i = 0; i < N_PLAYERS; i++){
+            playerWins.set(i, playerWins.get(i)/N_SIMULATIONS); 
+        }
+        return playerWins;
     }
     
     
